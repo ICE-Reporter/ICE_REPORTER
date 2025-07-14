b789fe272ffe4380d9b11fac5835fdc81619ec0c
Fly Dev <dev@fly.io>
2025-07-14 23:47:12 +0000

    add hcaptcha dependency

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule IceReporter.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"}
-    ]
+          {:hcaptcha, "~> 0.1.0"},
+]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
