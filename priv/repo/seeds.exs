23cb045d2fbfb697e40bde68b1768d2ca18e249c
ICE-Reporter <icereporter@proton.me>
2025-08-12 21:20:12 -0700

    Add proper US boundaries seed data for production

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,6 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     IceReporter.Repo.insert!(%IceReporter.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+
+# Load the production boundaries seed data
+Code.require_file("production_seeds.exs", __DIR__)
